664852cd5d8743b28666e32757eb035bbc0d960b
McGeachie <info@krakendigit.al>
2015-01-08 22:26:48 +0000

    BM: Some BASIC styling

--- app/scripts/losers/losers.controller.js
+++ app/scripts/losers/losers.controller.js
@@ -1,9 +1,5 @@
 var loserControllers = angular.module('loserControllers');
 
-loserControllers.controller('LoserCtrl', ['$scope', '$rootScope', 'losersDb', 'calculateBmi', function ($scope, $rootScope, losersDb, calculateBmi) {
+loserControllers.controller('LoserCtrl', ['$scope', '$rootScope', 'losersDb', function ($scope, $rootScope, losersDb) {
   $rootScope.losers = losersDb;
-
-  $scope.calculateBmi = function () {
-    $scope.bmi = calculateBmi($scope.weight, $scope.height);
-  }
 }]);
