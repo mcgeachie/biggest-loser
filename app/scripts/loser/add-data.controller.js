71d1f48e229d69dd00b881df0100103264481ae3
telly <telly@bskyb.com>
2015-01-13 16:04:03 +0000

    BM: Removing angular-ui chart

--- app/scripts/loser/add-data.controller.js
+++ app/scripts/loser/add-data.controller.js
@@ -1,6 +1,14 @@
 var loserControllers = angular.module('loserControllers');
 
 loserControllers.controller('AddDataCtrl', ['$scope', '$firebase', 'calculateBmi', function ($scope, $firebase, calculateBmi) {
+  var differenceFromStartWeight = function (weight) {
+    var startWeight = $scope.$parent.loser.startWeight.kg,
+    onePercentOfStart = startWeight / 100,
+    kgDifference = startWeight - weight;
+
+    return -1 * Math.round((kgDifference / onePercentOfStart) * 10 ) / 10;
+  };
+
   $scope.today = new Date();
   $scope.dateOptions = { startingDay: 1 };
 
@@ -21,7 +29,8 @@ loserControllers.controller('AddDataCtrl', ['$scope', '$firebase', 'calculateBmi
     $scope.$parent.loser.weights.$add({
       kg: $scope.weight,
       datePoint: $scope.date.toString(),
-      bmi: calculateBmi($scope.weight, $scope.$parent.loser.height)
+      bmi: calculateBmi($scope.weight, $scope.$parent.loser.height),
+      difference: differenceFromStartWeight($scope.weight)
     })
 
     $scope.cancelForm();
