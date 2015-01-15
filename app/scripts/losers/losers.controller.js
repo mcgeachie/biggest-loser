98c37f4c22f07a844eb289b20e88c470f1acc730
telly <telly@bskyb.com>
2015-01-15 14:40:12 +0000

    BM: Programmatic creation of graph datasets.

--- app/scripts/losers/losers.controller.js
+++ app/scripts/losers/losers.controller.js
@@ -1,6 +1,10 @@
 var loserControllers = angular.module('loserControllers');
 
 loserControllers.controller('LosersCtrl', ['$scope', '$rootScope', '$q', '$modal', 'losersDb', function ($scope, $rootScope, $q, $modal, losersDb) {
+  var sortDate = function (date, otherDate) {
+          return moment(date).isAfter(otherDate) ? 1 : -1;
+      };
+
   $rootScope.losers = losersDb;
 
   $rootScope.losers.$loaded(function () {
@@ -29,7 +33,7 @@ loserControllers.controller('LosersCtrl', ['$scope', '$rootScope', '$q', '$modal
   };
 
   $scope.renderChart = function () {
-    var ctx = document.getElementById("myChart").getContext("2d"),
+    var ctx = document.getElementById("loserChart").getContext("2d"),
         defaults = {
           strokeColor: "rgba(255, 255, 255, 0.5)",
           pointColor: "rgba(255, 255, 255, 1)",
@@ -37,76 +41,100 @@ loserControllers.controller('LosersCtrl', ['$scope', '$rootScope', '$q', '$modal
           pointHighlightFill: "#f94931",
           pointHighlightStroke: "rgba(255, 255, 255, 1)"
         },
-        sortDate = function (date, otherDate) {
-          return moment(date).isAfter(otherDate) ? 1 : -1;
-        },
         labels = [],
         datasets = [];
 
-        $scope.losers.forEach(function (loser) {
-          var dataset = jQuery.extend({}, defaults, {
-                label: loser.name,
-                data: []
-              });
+    $scope.losers.forEach(function (loser) {
 
-          for (var dateKey in loser.weights) {
+      for (var dateKey in loser.weights) {
+        var date = moment(loser.weights[dateKey].datePoint).format('DD/MM/YYYY');
 
-            var date = moment(loser.weights[dateKey].datePoint).format('DD/MM/YYYY');
+        if (labels.indexOf(date) == -1) labels.push(date);
+      }
+    });
 
-            if (labels.indexOf(date) == -1) labels.push(date);
+    labels.sort(sortDate);
+
+    $scope.losers.forEach(function (loser) {
+
+      var dataset = jQuery.extend({}, defaults, {
+              label: loser.name,
+              initials: loser.initials,
+              data: []
+          });
+
+      for (var i = 0; i < labels.length; i++) {
+        var correspondingDataFound,
+            diff = 0;
+
+        for (var weightKey in loser.weights) {
+
+          if (moment(loser.weights[weightKey].datePoint).format('DD/MM/YYYY') == labels[i]) {
+            correspondingDataFound = true;
+            diff = loser.weights[weightKey].diff;
+            break;
           }
-
-          labels.sort(sortDate);
-
-          console.log(labels);
-
-          for (var i = 0; i < labels.length; i++) {
-
-            var diff = 0;
-
-            console.groupCollapsed(labels[i]);
-
-            for (var weightKey in loser.weights) {
-              var formattedDate = moment(loser.weights[weightKey].datePoint).format('DD/MM/YYYY')
-
-              console.log('datePoint', formattedDate);
-              console.log('label', labels[i]);
-
-              console.log('equals', formattedDate == labels[i])
-
-              if (formattedDate == labels[i]) {
-                diff = loser.weights[weightKey].diff
-              }
-
-              dataset.data.push(diff);
-            }
-
-            console.groupEnd();
-          }
-
-          // console.log(dataset);
-
-          datasets.push(dataset);
-        });
-
-        //add date label one week after last available date
-
-        console.log('after all losers', labels);
-
-        data = {
-          labels: labels,
-          datasets: datasets
         };
-    //TODO: loop through data and create dates programatically then match datapoints to dates whilst looping and preserve indices
+
+        dataset.data.push(diff);
+      };
+
+      datasets.push(dataset);
+    });
+
+    labels.push(moment(labels[labels.length - 1], 'DD/MM/YYYY').add(1, 'weeks').format('DD/MM/YYYY'));
+
+    data = {
+      labels: labels,
+      datasets: datasets
+    };
+
+    function renderAvatars() {
+
+      var findInitials = function (name) {
+        var processedName = name.replace('\'', ''), //For anyone who thinks an apostrophe is valid in a NAME
+        initialsArray = processedName.match(/\b(\w)/g),
+        initials;
+
+        if (initialsArray.length > 1) {
+          initials = initialsArray[0] + initialsArray[initialsArray.length - 1];
+        }
+
+        return initials ? initials.toUpperCase() : initials;
+      };
+
+      $scope.chart.datasets.forEach(function (dataset) {
+        var latestPoint = dataset.points[dataset.points.length - 1],
+            avatarImage = new Image(),
+            initials = findInitials(dataset.label);
+
+        console.groupCollapsed(dataset.label);
+        console.log(dataset);
+        console.log(latestPoint.x);
+        console.log(latestPoint.y);
+        console.groupEnd();
+
+        avatarImage.onload = function () {
+          if (initials) ctx.drawImage(avatarImage, latestPoint.x - 20, latestPoint.y - 20, 40, 40);
+        };
+
+        avatarImage.onerror = function () {
+          avatarImage.src = '/images/grumpy-fat-cat.png';
+        };
+
+        avatarImage.src = 'http://avatar.faw.bskyb.com/display/' + initials;
+      });
+    };
 
     $scope.chart = new Chart(ctx).Line(data, {
+      // onAnimationComplete: function () { renderAvatars() }
       scaleLineColor: "rgba(255, 255, 255, 0.5)",
       scaleGridLineColor : "rgba(255, 255, 255, 0.1)",
       scaleFontColor: "#fff",
       scaleLabel: "<%=value%> %",
       showTooltips: false,
       responsive: true,
-      datasetFill: false
+      datasetFill: false,
     });
   };
 }]);
